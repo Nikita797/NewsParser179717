0efa3203bd68ab64094febfd439ce6eb5204eaca
nekit797 <n717@bk.ru>
2021-06-17 19:24:03 +0300

    Initial commit

---
+++ ConsoleApp3/NewsParser.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using System.Linq;
+using AngleSharp;
+
+namespace Awesome
+{
+    public static class NewsParser
+    {
+        public static async Task<string> FindNews(string text)
+        {
+            var context = BrowsingContext.New(Configuration.Default.WithDefaultLoader());
+            var document = await context.OpenAsync($"https://dtf.ru/search/v2/content/new?query={text}");
+            var elements = document.QuerySelectorAll("div.content-title.content-title--short.l-island-a");
+            var arr = elements.Select(it => it.TextContent).Take(5);
+            return string.Join('\n', arr);
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+
+namespace Awesome 
+{
+  class Program
+  {
+    static ITelegramBotClient botClient;
+
+    static void Main()
+    {
+      botClient = new TelegramBotClient("1843129045:AAGjJ8lSf95nDbP9j44UmnNQnf2urqC5ioo");
+
+      var me = botClient.GetMeAsync().Result;
+      Console.WriteLine(
+        $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+      );
+
+      botClient.OnMessage += Bot_OnMessage;
+      botClient.StartReceiving();
+
+      Console.WriteLine("Press any key to exit");
+      Console.ReadKey();
+
+      botClient.StopReceiving();
+    }
+    static async void Bot_OnMessage(object sender, MessageEventArgs e)
+    {
+      if (e.Message.Text == null) return;
+      await botClient.SendTextMessageAsync(chatId: e.Message.Chat, text: await NewsParser.FindNews(e.Message.Text));
+    }
+  }
+}
+  
+  
